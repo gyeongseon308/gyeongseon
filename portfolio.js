964d7830cee103411e366e4de2ca62023779e673
gyeongseon308 <zooin308@gmail.com>
2024-12-10 00:23:59 +0900

    모바일 수정4

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,51 @@
-$(function(){
+$(document).ready(function () {
+    $(".hamburger_icon").click(function () {
+        $(".column_box").stop().fadeIn(400);
+    });
+
+    $(".close").click(function () {
+        $(".column_box").stop().fadeOut(400);
+    });
+
+    $(".link_p").click(function () {
+        let SubMenu = $(this).siblings(".hamburg_sub");
+        let OpenIcon = $(this).find(".open");
+        let ShutIcon = $(this).find(".shut");
+
+        if (SubMenu.is(":visible")) {
+            SubMenu.slideUp();
+            OpenIcon.show();
+            ShutIcon.hide();
+        } else {
+            SubMenu.slideDown();
+            OpenIcon.hide();
+            ShutIcon.show();
+        }
+    });
+
+    $(".hamburg_sub>li").mouseenter(function () {
+        $(".link_p").css({ "color": "#dd4f5f" });
+        $(".link_p>a").css({ "color": "#dd4f5f" });
+    })
+    $(".hamburg_sub>li").mouseleave(function () {
+        $(".link_p").css({ "color": "" });
+        $(".link_p>a").css({ "color": "" });
+    });
+});
+
+
+$(function () {
     document.addEventListener('mousemove', (e) => {
         let mouseX = e.pageX + 5; // document의 x좌표값
         let mouseY = e.pageY + 5; // document의 y좌표값
-    
+
         let cursor = document.querySelector('.cursor');
         cursor.style.left = mouseX + 'px';
         cursor.style.top = mouseY + 'px';
     });
 });
 
-$(function(){
+$(function () {
     const sections = ["section1", "section2", "section3", "section4", "section5"]
     const positions = {};
 
@@ -19,17 +55,17 @@ $(function(){
         }
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         let scrollTop = $(this).scrollTop();
-        
-        if (scrollTop >= positions["section3"] && scrollTop < positions["section4"]){
+
+        if (scrollTop >= positions["section3"] && scrollTop < positions["section4"]) {
             $("#section3>.section3_nav").stop(true, true).fadeIn();
-        } else{
+        } else {
             $("#section3>.section3_nav").stop(true, true).fadeOut();
         }
-});
-    $(function(){
-        let list =  [".p1", ".p2", ".p3", ".p4", ".p5", ".p6", ".p7"]
+    });
+    $(function () {
+        let list = [".p1", ".p2", ".p3", ".p4", ".p5", ".p6", ".p7"]
         let projects = {};
 
         list.forEach(item => {
@@ -39,16 +75,24 @@ $(function(){
             }
         });
 
-        $(".section3_nav>ul>li").click(function(){
+        $(".hamburg_sub>li").click(function () {
             let targetClass = $(this).data("target");
             let scroll = projects[targetClass];
 
-            if (scroll !== undefined){
+            if (scroll !== undefined) {
+                $("html,body").stop().animate({ scrollTop: scroll }, 100);
+            }
+        });
+        $(".section3_nav>ul>li").click(function () {
+            let targetClass = $(this).data("target");
+            let scroll = projects[targetClass];
+
+            if (scroll !== undefined) {
                 $("html,body").stop().animate({ scrollTop: scroll }, 100);
             }
         });
     });
-    $(".quick_menu>.quick_btn").click(function(){
+    $(".quick_menu>.quick_btn").click(function () {
         $(".quick_menu>.quick_sub").slideToggle(500);
     });
 });
